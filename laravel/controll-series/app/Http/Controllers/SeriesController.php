8e2e51f73d3c64aacfc28ef39f2046d1c46f6936
Lucas Ribeiro <lucas.daniel@gmail.com>
2024-01-19 00:58:19 -0500

    add route to list the series

---
+++ laravel/controll-series/app/Http/Controllers/SeriesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SeriesController extends Controller
+{
+    public function index()
+    {
+        $series = ['Punisher', 'Lost', 'Grey\'s Anatomy'];
+        $html = '<ul>';
+        foreach ($series as $serie) {
+            $html .= "<li>$serie</li>";
+        }
+        $html .= '</ul>';
+
+        echo $html;
+    }
+}
