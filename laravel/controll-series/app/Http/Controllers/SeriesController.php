93c7bb239ab2f31b63fa3228323624cb1286b69f
Lucas Ribeiro <lucas.daniel@gmail.com>
2024-01-19 01:26:33 -0500

    refactor to use component view

--- laravel/controll-series/app/Http/Controllers/SeriesController.php
+++ laravel/controll-series/app/Http/Controllers/SeriesController.php
@@ -9,6 +9,8 @@ class SeriesController extends Controller
     public function index()
     {
         $series = ['Punisher', 'Lost', 'Grey\'s Anatomy'];
-        return view('series-list', ['series' => $series]);
+        // return view('series-list', ['series' => $series]);
+        // return view('series-list', compact('series'));
+        return view('series.index')->with('series', $series);
     }
 }
