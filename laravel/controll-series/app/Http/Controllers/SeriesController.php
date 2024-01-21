cce2bf85185ee957cb441465f2d93d599b45c1f4
Lucas Ribeiro <lucas.daniel@gmail.com>
2024-01-20 21:57:40 -0500

    add db transaction

--- laravel/controll-series/app/Http/Controllers/SeriesController.php
+++ laravel/controll-series/app/Http/Controllers/SeriesController.php
@@ -7,6 +7,7 @@ use App\Models\Episode;
 use App\Models\Season;
 use App\Models\Series;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SeriesController extends Controller
 {
@@ -34,30 +35,35 @@ class SeriesController extends Controller
         //     'name' => ['required', 'min:3']
         // ]);
 
-        $series = Series::create($request->all());
-        $seasons = [];
-        for ($i = 1; $i <= $request->seasonsQty; $i++) {
-            $seasons[] = [
-                'series_id' => $series->id,
-                'number' => $i,
-            ];
-        }
-
-        // insert seasons
-        Season::insert($seasons);
-
-        $episodes = [];
-        foreach ($series->seasons as $season) {
-            for ($i = 1; $i <= $request->episodesQty; $i++) {
-                $episodes[] = [
-                    'season_id' => $season->id,
+        $series = DB::transaction(function () use ($request) {
+            $series = Series::create($request->all());
+            $seasons = [];
+            for ($i = 1; $i <= $request->seasonsQty; $i++) {
+                $seasons[] = [
+                    'series_id' => $series->id,
                     'number' => $i,
                 ];
             }
-        }
 
-        // insert episodes
-        Episode::insert($episodes);
+            // insert seasons
+            Season::insert($seasons);
+
+            $episodes = [];
+            foreach ($series->seasons as $season) {
+                for ($i = 1; $i <= $request->episodesQty; $i++) {
+                    $episodes[] = [
+                        'season_id' => $season->id,
+                        'number' => $i,
+                    ];
+                }
+            }
+
+            // insert episodes
+            Episode::insert($episodes);
+
+            return $series;
+        });
+
 
         // with: add flash message
         return to_route('series.index')->with('message.success', "Series '{$series->name}' created successfully");
